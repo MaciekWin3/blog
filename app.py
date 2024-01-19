505ebfa541738934e29283d82b198496fd44749b
Maciej Winnik <maciekwin3@gmail.com>
2024-01-19 18:25:27 +0100

    Add article endpoint

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
-from flask import Flask, render_template, render_template_string
+from flask import Flask, render_template, render_template_string, abort
 from markupsafe import escape
 from urllib.request import urlopen
 import markdown
-import sqlite3
+import requests
+
+user = "maciekwin3"
+repo = "blog"
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! </p>"
 
 @app.route("/")
 def template():
     return render_template("index.html")
 
-#def get_articles_from_github(): 
+@app.route("/article/<name>")
+def get_articles_from_github(name): 
+    url = "https://api.github.com/repos/{}/{}/git/trees/main?recursive=1".format(user, repo)
+    url_raw = "https://raw.githubusercontent.com/MaciekWin3/blog/main/"
+    r = requests.get(url)
+    res = r.json()
+    print(res)
 
-@app.route('/call')
-def get_html_content():
-    text = """
-    write me markdown with image
-    ![alt text](https://raw.githubusercontent.com/MaciekWin3/Moonwalk/master/docs/logo.png)
-    """
-    url = 'https://raw.githubusercontent.com/MaciekWin3/TermKeyVault/master/README.md'
-    
-    try:
-        # Make an HTTP request to the URL and read the HTML content
-        with urlopen(url) as response:
-            html_content = response.read().decode('utf-8')
-        
-        # Parse the HTML content as Markdown
-        markdown_content = markdown.markdown(html_content)
-        
-        return render_template_string(markdown_content)
-    except Exception as e:
-        return f"Failed to retrieve or parse content. Error: {str(e)}"
+    for file in res["tree"]:
+        if file["path"].endswith(".md") and file["path"] != "README.md":
+            if file["path"] == "articles/" + escape(name) + ".md":
+                content = requests.get(url_raw + file["path"])
+                md = markdown.markdown(content.text)
+                return render_template_string(md)
+            else:
+                continue
+
+    abort(404)
